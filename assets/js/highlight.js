b4d5a6823731ef15531f26f00b637d36e666009f
Dmitry Kovalev <dakovalev1@mail.ru>
2019-04-17 11:24:53 +0300

    highlight.js fix

--- assets/js/highlight.js
+++ assets/js/highlight.js
@@ -1,17 +1,34 @@
+function getDocHeight() {
+    var D = document;
+    return Math.max(
+        D.body.scrollHeight, D.documentElement.scrollHeight,
+        D.body.offsetHeight, D.documentElement.offsetHeight,
+        D.body.clientHeight, D.documentElement.clientHeight
+    );
+}
+
 function updateClass(){
+    max_index = 0;
+    
     $(".nav-link-section").each(function(index){
         section = $($(this).attr("href"))
         offset = section.offset().top - $(window).scrollTop() - parseInt($("body").css("margin-top"), 10);
 
-        if (offset <= 3){
-            $(".nav-link-section").removeClass("active");
+        if (offset <= 3 && max_index < index){
+            max_index = index;
+        }
+        
+    });
+    
+    $(".nav-link-section").each(function(index){
+        if(index== max_index){
             $(this).addClass("active");
-        } else {
+        }else{
             $(this).removeClass("active");
         }
     });
     
-    if($(window).scrollTop() + $(window).height() >= $(document).height()){
+    if($(window).scrollTop() + $(window).height() >= getDocHeight()) {
         $(".nav-link-section").removeClass("active");
         $(".nav-link-section").last().addClass("active");
     }

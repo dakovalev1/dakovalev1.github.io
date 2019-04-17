105bdb279c8c66f69026747871814887548d96b8
Dmitry Kovalev <dakovalev1@mail.ru>
2019-04-17 11:08:19 +0300

    added last section highlight

--- assets/js/highlight.js
+++ assets/js/highlight.js
@@ -1,23 +1,20 @@
 function updateClass(){
-    max_index = 0;
-    
     $(".nav-link-section").each(function(index){
         section = $($(this).attr("href"))
         offset = section.offset().top - $(window).scrollTop() - parseInt($("body").css("margin-top"), 10);
 
-        if (offset <= 3 && max_index < index){
-            max_index = index;
-        }
-        
-    });
-    
-    $(".nav-link-section").each(function(index){
-        if(index== max_index){
+        if (offset <= 3){
+            $(".nav-link-section").removeClass("active");
             $(this).addClass("active");
-        }else{
+        } else {
             $(this).removeClass("active");
         }
     });
+    
+    if($(window).scrollTop() + $(window).height() >= $(document).height()){
+        $(".nav-link-section").removeClass("active");
+        $(".nav-link-section").last().addClass("active");
+    }
 }
 
 $(document).ready(function(){

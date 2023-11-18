edcc3433b06d901dd6b2e85f0b6337580b2518a3
Dmitry Kovalev <dakovalev1@gmail.com>
2023-11-18 16:39:34 +0300

    JS update

--- assets/js/highlight.js
+++ assets/js/highlight.js
@@ -1,32 +1,33 @@
-function updateClass(){
+function updateClass() {
     max_index = 0;
-    
-    $(".nav-link-section").each(function(index){
+    contact_index = 0;
+
+    $(".nav-link-section").each(function (index) {
         section = $($(this).attr("href"))
         offset = section.offset().top - $(window).scrollTop() - parseInt($("body").css("margin-top"), 10);
 
-        if (offset <= 3 && max_index < index){
+        if (offset <= 3 && max_index < index) {
             max_index = index;
         }
-        
+
     });
-    
-    if ($('.bottom').offset().top <= $(window).scrollTop() + (window.innerHeight || $(window).height())){
+
+    if ($('.bottom').offset().top <= $(window).scrollTop() + (window.innerHeight || $(window).height())) {
         max_index = $('.nav-link-section').length - 1;
     }
-    
-    $(".nav-link-section").each(function(index){
-        if(index == max_index){
+
+    $(".nav-link-section").each(function (index) {
+        if (index == max_index) {
             $(this).addClass("active");
-        }else{
+        } else {
             $(this).removeClass("active");
         }
-    }); 
+    });
 }
 
-$(document).ready(function(){
+$(document).ready(function () {
     updateClass();
-    $(document).scroll(function(){
+    $(document).scroll(function () {
         updateClass();
     });
 });

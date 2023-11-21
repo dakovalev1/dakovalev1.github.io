918fdae30902fdcb22661acb05e7495a218e05d2
Dmitry Kovalev <dakovalev1@yandex-team.ru>
2023-11-21 19:27:32 +0300

    bibtex

--- TEST2/main2.py
+++ TEST2/main2.py
@@ -1,55 +1,62 @@
 import bibtexparser
 import bibtexparser.middlewares
 import bibtexparser.middlewares.latex_encoding
-import bibtexparser.library
-import bibtexparser.entrypoint
-import bibtexparser.model
-import bibtexparser.middlewares.names
+import json
+import re
 
 import jinja2
 
 
 class Paper:
-    def __init__(self, title, authors) -> None:
+    def __init__(self, title, authors, venue, year) -> None:
         self.title = title
         self.authors = authors
-                                    
+        self.venue = venue
+        self.year = int(year)
+
 
 b = bibtexparser.parse_file('papers.bib', append_middleware=[
     bibtexparser.middlewares.LatexDecodingMiddleware(decoder=bibtexparser.middlewares.latex_encoding.LatexNodes2Text()),
     bibtexparser.middlewares.SeparateCoAuthors(True),
     bibtexparser.middlewares.SplitNameParts(True),
     bibtexparser.middlewares.MergeNameParts(True),
-    bibtexparser.middlewares.MergeCoAuthors(),
     bibtexparser.middlewares.SortFieldsAlphabeticallyMiddleware()
 ])
 
 
-print('START')
+
+author_json = json.load(open('authors.json'))
 
 paper_list = []
 
 
-print(b.entries[10].fields_dict['year'].value)
+
 
 for entry in b.entries:
     fields = entry.fields_dict
-    # print(fields['title'].value)
-    # print('\t', fields['author'].value)
-    paper_list.append(Paper(fields['title'].value, fields['author'].value))
-    # for author in fields['author'].value:
-    #     print('\t', author.merge_first_name_first)
+    venue = ''
+
+    if 'journal' in fields.keys():
+        venue = fields['journal'].value
+    elif 'booktitle' in fields.keys():
+        venue = fields['booktitle'].value
+    else:
+        raise KeyError('no venue', fields)
+    
+    authors = fields['author'].value
+
+    for index, a in enumerate(authors):
+        if a in author_json:
+            authors[index] = '<a href={}>{}</a>'.format(author_json[a], a)
+        
+
+    paper_list.append(Paper(fields['title'].value, ', '.join(authors), venue, fields['year'].value))
+    
 
 
 file_loader = jinja2.FileSystemLoader('')
 env = jinja2.Environment(loader=file_loader)
 template = env.get_template('template_index.html')
 
-output = template.render(papers = paper_list)
+output = template.render(publications = paper_list)
 open('index.html', 'w').write(output)
-
-
-# a = d['author'].value[3]
-
-
-# print(a.merge_first_name_first)

9c1c1a85207cffc9da446022818841be63cbe6eb
Dmitry Kovalev <dakovalev1@yandex-team.ru>
2024-08-13 18:34:55 +0300

    trigger nav collapse

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,2 +1,9 @@
-$(document).ready(function(){
+$(document).ready(function () {
 });
+
+function navbar_link_click() {
+    let navbar_toggler = document.querySelector(".navbar-toggler");
+    if (getComputedStyle(navbar_toggler).getPropertyValue("display") != 'none') {
+        navbar_toggler.click();
+    }
+}
